042b966acf63a6fcfe4911aed571c9738cacd439
Chandra-Bhan <121319182+Chandra-Bhan@users.noreply.github.com>
2023-06-05 19:24:09 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './style.css';
 import Student from './Student';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//create a component student form and add four input boxes and collect student information Create a submit button and on click of submit button, read all the values from the form and show it in a table
 export default function App() {
   return (
     <div>
